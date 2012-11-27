08f1e51357dd36510fc50c2bd60df24f3f050d00
Kody Brown <kody.brown@venafi.com>
2012-11-27 15:16:28 -0700

    - Initial commit.

---
+++ shrink.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bricksoft.PowerCode;
+
+namespace Bricksoft.DosToys
+{
+	public class shrink
+	{
+		public static void Main( string[] args )
+		{
+			// Adjust the window size.
+			Console.WindowWidth = 80;
+			Console.WindowHeight = 25;
+
+			Console.BufferWidth = 80;
+			Console.BufferHeight = 3000;
+
+			ConsoleUtils.CenterWindow();
+		}
+	}
+}
